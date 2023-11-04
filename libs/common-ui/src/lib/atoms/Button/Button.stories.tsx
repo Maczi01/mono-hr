1bc1746a6544598f4ee9c1caade2fe706ff1fc71
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-04 19:43:06 +0000

    chore: added props to Button.tsx

--- libs/common-ui/src/lib/atoms/Button/Button.stories.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.stories.tsx
@@ -22,7 +22,7 @@ export const Primary: Story = {
   args: {
     label: 'Hello',
     bgColor: 'amethyst',
-    color: 'emerald',
+    color: 'primary',
   },
 };
 
@@ -30,6 +30,6 @@ export const Secondary: Story = {
   args: {
     label: 'Hello',
     bgColor: 'peter-river',
-    color: 'amethyst',
+    color: 'secondary',
   },
 };
--- libs/common-ui/src/lib/atoms/Button/Button.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.tsx
@@ -1,7 +1,29 @@
-type Props = {
-  label: string;
+const colors = {
+  amethyst: '#9b59b6',
+  'peter-river': '#3498db',
+  emerald: '#2ecc71',
 };
 
-export const Button = ({ label }: Props) => {
-  return <button>{label}</button>;
+type ColorType = keyof typeof colors;
+
+type Props = {
+  label: string;
+  bgColor?: ColorType;
+  color?: ColorType;
+};
+
+export const Button = ({ label, bgColor, color }: Props) => {
+  const _bgColor = bgColor ? colors[bgColor] : '';
+  const _color = color ? colors[color] : '';
+
+  return (
+    <button
+      style={{
+        backgroundColor: _bgColor,
+        color: _color,
+      }}
+    >
+      {label}
+    </button>
+  );
 };
