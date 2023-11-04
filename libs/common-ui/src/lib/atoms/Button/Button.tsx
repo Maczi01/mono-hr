bd7a4924383a1f6740da240e610b931939ed332a
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-04 20:17:20 +0000

    fix: added tailwind classes to Button.tsx

--- libs/common-ui/src/lib/atoms/Button/Button.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.tsx
@@ -1,4 +1,5 @@
 import type { ComponentProps } from 'react';
+import { clsx } from 'clsx';
 
 const colors = {
   amethyst: '#9b59b6',
@@ -12,14 +13,32 @@ type ButtonProps = {
   label: string;
   bgColor?: ColorType;
   color?: ColorType;
+  className?: string;
 } & ComponentProps<'button'>;
 
-export const Button = ({ label, bgColor, color, ...rest }: ButtonProps) => {
+export const Button = ({
+  label,
+  bgColor,
+  color,
+  className,
+  ...rest
+}: ButtonProps) => {
   const _bgColor = bgColor ? colors[bgColor] : '';
   const _color = color ? colors[color] : '';
 
+  const classes = clsx(
+    'px-4 py-1',
+    'text-sm text-white',
+    'bg-blue-600',
+    'font-semibold',
+    'rounded-full border border-blue-200',
+    'hover:bg-blue-700 hover:border-transparent hover:text-white',
+    className
+  );
+
   return (
     <button
+      className={classes}
       style={{
         backgroundColor: _bgColor,
         color: _color,
