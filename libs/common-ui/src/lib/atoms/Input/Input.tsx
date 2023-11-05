1ad58c5df74592a329c640c0213cba533896e4f5
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-05 18:25:47 +0000

    fix: wrapped Input.tsx with forwardRef

--- libs/common-ui/src/lib/atoms/Input/Input.tsx
+++ libs/common-ui/src/lib/atoms/Input/Input.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, useEffect, useId, useState } from 'react';
-import type { ComponentProps, ChangeEventHandler } from 'react';
+import { ChangeEvent, forwardRef, useEffect, useId, useState } from 'react';
+import type { ComponentProps, ChangeEventHandler, Ref } from 'react';
 import { clsx } from 'clsx';
 import { Label } from '../Label';
 
@@ -10,51 +10,50 @@ type InputProps = {
   helperText?: string;
 } & ComponentProps<'input'>;
 
-export const Input = ({
-  label,
-  value,
-  className,
-  helperText,
-  onChange,
-  ...rest
-}: InputProps) => {
-  const id = useId();
+export const Input = forwardRef(
+  (
+    { label, value, className, helperText, onChange, ...rest }: InputProps,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    const id = useId();
 
-  const classesInput = clsx(
-    'peer h-10 w-full',
-    'bg-gray-50',
-    'px-4',
-    'rounded-full border border-blue-200',
-    'font-thin outline-none drop-shadow-sm',
-    'transition-all duration-200',
-    'ease-in-out',
-    'focus:bg-white focus:ring-2 focus:ring-blue-400',
-    className
-  );
-  const classesHelperText = clsx(
-    'absolute',
-    'block',
-    'pt-1',
-    'text-xs font-semibold text-gray-500',
-    'opacity-0',
-    'transition-all duration-200',
-    'ease-in-out',
-    'group-focus-within:opacity-100',
-    className
-  );
+    const classesInput = clsx(
+      'peer h-10 w-full',
+      'bg-gray-50',
+      'px-4',
+      'rounded-full border border-blue-200',
+      'font-thin outline-none drop-shadow-sm',
+      'transition-all duration-200',
+      'ease-in-out',
+      'focus:bg-white focus:ring-2 focus:ring-blue-400',
+      className
+    );
+    const classesHelperText = clsx(
+      'absolute',
+      'block',
+      'pt-1',
+      'text-xs font-semibold text-gray-500',
+      'opacity-0',
+      'transition-all duration-200',
+      'ease-in-out',
+      'group-focus-within:opacity-100',
+      className
+    );
 
-  return (
-    <div className="group relative w-72 md:w-80 lg:w-96">
-      {label && <Label id={id}>{label}</Label>}
-      <input
-        className={classesInput}
-        type="text"
-        id={id}
-        defaultValue={value}
-        onChange={onChange}
-        {...rest}
-      />
-      {helperText && <span className={classesHelperText}>{helperText}</span>}
-    </div>
-  );
-};
+    return (
+      <div className="group relative w-72 md:w-80 lg:w-96">
+        {label && <Label id={id}>{label}</Label>}
+        <input
+          ref={ref}
+          className={classesInput}
+          type="text"
+          id={id}
+          defaultValue={value}
+          onChange={onChange}
+          {...rest}
+        />
+        {helperText && <span className={classesHelperText}>{helperText}</span>}
+      </div>
+    );
+  }
+);
