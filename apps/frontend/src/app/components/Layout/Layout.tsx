d814be9b98ee2dd3b0e76e07d38bf9c44e8f3039
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-07 19:20:01 +0000

    feat: added links

--- apps/frontend/src/app/components/Layout/Layout.tsx
+++ apps/frontend/src/app/components/Layout/Layout.tsx
@@ -1,9 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
     <div>
-      <nav>Welcome to Layout!</nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home Page</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Page</Link>
+          </li>
+          <li>
+            <Link to="/about">About Page</Link>
+          </li>
+          <li>
+            <Link to="/employee">Employee Page</Link>
+          </li>
+        </ul>
+      </nav>
       <Outlet />
       <footer>Welcome to Layout!</footer>
     </div>
