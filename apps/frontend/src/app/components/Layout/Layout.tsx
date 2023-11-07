16c426d8f55f62da04c5726ee6410dc0af45ba6b
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-07 19:34:31 +0000

    feat: extracted menu / navbar

--- apps/frontend/src/app/components/Layout/Layout.tsx
+++ apps/frontend/src/app/components/Layout/Layout.tsx
@@ -4,22 +4,6 @@ import { ROUTE } from '../../../routes';
 export const Layout = () => {
   return (
     <div>
-      <nav>
-        <ul className="flex list-none">
-          <li className="mx-2">
-            <Link to={ROUTE.HOME}>Home Page</Link>
-          </li>
-          <li className="mx-2">
-            <Link to={ROUTE.CONTACT}>Contact Page</Link>
-          </li>
-          <li className="mx-2">
-            <Link to={ROUTE.ABOUT}>About Page</Link>
-          </li>
-          <li className="mx-2">
-            <Link to={ROUTE.EMPLOYEES}>Employee Page</Link>
-          </li>
-        </ul>
-      </nav>
       <Outlet />
       <footer>Welcome to footer!</footer>
     </div>
