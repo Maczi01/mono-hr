9f4d59eed69d0ad62ed86284ec03ba43084ca871
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-07 19:17:57 +0000

    chore: removed unused NxWelcome component

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,9 +1,32 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { ContactPage } from './pages/ContactPage';
+import { AboutPage } from './pages/AboutPage';
+import { HomePage } from './pages/HomePage';
+import { EmployeesPage } from './pages/EmployeesPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div> Hello world</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+      {
+        path: '/about',
+        element: <AboutPage />,
+      },
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/employee',
+        element: <EmployeesPage />,
+      },
+    ],
   },
 ]);
 
