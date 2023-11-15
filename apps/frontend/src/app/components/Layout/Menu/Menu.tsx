1d571fd7035a8432a8a595e4f29315d666ea0f52
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-15 22:03:37 +0000

    feat: created header with logos

--- apps/frontend/src/app/components/Layout/Menu/Menu.tsx
+++ apps/frontend/src/app/components/Layout/Menu/Menu.tsx
@@ -1,28 +1,37 @@
 import { Link } from 'react-router-dom';
 import { ROUTE } from '../../../../routes';
+import logo from '../Menu/logo.svg';
+import exit from '../Menu/exit.svg';
 
 export const Menu = () => {
   return (
-    <div>
-      <nav>
-        <ul className="flex list-none">
-          <li className="mx-2">
-            <Link to={ROUTE.HOME}>Home Page</Link>
-          </li>
-          <li className="mx-2">
-            <Link to={ROUTE.CONTACT}>Contact Page</Link>
-          </li>
-          <li className="mx-2">
-            <Link to={ROUTE.ABOUT}>About Page</Link>
-          </li>
-          <li className="mx-2">
-            <Link to={ROUTE.EMPLOYEES}>Employee Page</Link>
-          </li>
-          <li className="mx-2">
-            <Link to={ROUTE.REGISTER}>Registration Page</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="bg-amber-400">
+      <div className="max-w-full flex flex-wrap items-center justify-between mx-auto px-8 py-2">
+        <div className="flex items-center">
+          <img src={logo} alt="Logout" style={{ width: '40px' }} />
+          <ul className="flex justify-between list-none ml-4">
+            <li className="mx-2">
+              <Link to={ROUTE.HOME}>Home Page</Link>
+            </li>
+            <li className="mx-2">
+              <Link to={ROUTE.CONTACT}>Contact Page</Link>
+            </li>
+            <li className="mx-2">
+              <Link to={ROUTE.ABOUT}>About Page</Link>
+            </li>
+            <li className="mx-2">
+              <Link to={ROUTE.EMPLOYEES}>Employee Page</Link>
+            </li>
+            <li className="mx-2">
+              <Link to={ROUTE.REGISTER}>Registration Page</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <img src={exit} alt="Logout" style={{ width: '40px' }} />
+        </div>
+      </div>
+    </nav>
   );
 };
