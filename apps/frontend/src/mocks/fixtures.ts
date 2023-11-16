92db8a73f0ff4c6ee9bc6bb9041bf4a001278727
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-16 22:48:36 +0000

    feat: added more data to fixtures.ts

--- apps/frontend/src/mocks/fixtures.ts
+++ apps/frontend/src/mocks/fixtures.ts
@@ -15,4 +15,268 @@ export const data = [
     },
     email: 'Matti@te.com',
   },
+  {
+    id: 3,
+    name: {
+      first: 'Alice',
+      last: 'Johnson',
+    },
+    email: 'alice@johnson.com',
+  },
+  {
+    id: 4,
+    name: {
+      first: 'Bob',
+      last: 'Smith',
+    },
+    email: 'bob@smith.com',
+  },
+  {
+    id: 5,
+    name: {
+      first: 'Emily',
+      last: 'Davis',
+    },
+    email: 'emily@davis.com',
+  },
+  {
+    id: 6,
+    name: {
+      first: 'Alex',
+      last: 'Turner',
+    },
+    email: 'alex@turner.com',
+  },
+  {
+    id: 7,
+    name: {
+      first: 'Sara',
+      last: 'Williams',
+    },
+    email: 'sara@williams.com',
+  },
+  {
+    id: 8,
+    name: {
+      first: 'Michael',
+      last: 'Johnson',
+    },
+    email: 'michael@johnson.com',
+  },
+  {
+    id: 9,
+    name: {
+      first: 'Sophia',
+      last: 'Brown',
+    },
+    email: 'sophia@brown.com',
+  },
+  {
+    id: 10,
+    name: {
+      first: 'Daniel',
+      last: 'Miller',
+    },
+    email: 'daniel@miller.com',
+  },
+  {
+    id: 11,
+    name: {
+      first: 'Olivia',
+      last: 'Jones',
+    },
+    email: 'olivia@jones.com',
+  },
+  {
+    id: 12,
+    name: {
+      first: 'Matthew',
+      last: 'Clark',
+    },
+    email: 'matthew@clark.com',
+  },
+  {
+    id: 13,
+    name: {
+      first: 'Ava',
+      last: 'Taylor',
+    },
+    email: 'ava@taylor.com',
+  },
+  {
+    id: 14,
+    name: {
+      first: 'David',
+      last: 'White',
+    },
+    email: 'david@white.com',
+  },
+  {
+    id: 15,
+    name: {
+      first: 'Emma',
+      last: 'Moore',
+    },
+    email: 'emma@moore.com',
+  },
+  {
+    id: 16,
+    name: {
+      first: 'Christopher',
+      last: 'Anderson',
+    },
+    email: 'christopher@anderson.com',
+  },
+  {
+    id: 17,
+    name: {
+      first: 'Isabella',
+      last: 'Thomas',
+    },
+    email: 'isabella@thomas.com',
+  },
+  {
+    id: 18,
+    name: {
+      first: 'Andrew',
+      last: 'Hill',
+    },
+    email: 'andrew@hill.com',
+  },
+  {
+    id: 19,
+    name: {
+      first: 'Madison',
+      last: 'Allen',
+    },
+    email: 'madison@allen.com',
+  },
+  {
+    id: 20,
+    name: {
+      first: 'Joseph',
+      last: 'Baker',
+    },
+    email: 'joseph@baker.com',
+  },
+  {
+    id: 21,
+    name: {
+      first: 'Chloe',
+      last: 'Green',
+    },
+    email: 'chloe@green.com',
+  },
+  {
+    id: 22,
+    name: {
+      first: 'Ethan',
+      last: 'Nelson',
+    },
+    email: 'ethan@nelson.com',
+  },
+  {
+    id: 23,
+    name: {
+      first: 'Grace',
+      last: 'Cooper',
+    },
+    email: 'grace@cooper.com',
+  },
+  {
+    id: 24,
+    name: {
+      first: 'William',
+      last: 'Ward',
+    },
+    email: 'william@ward.com',
+  },
+  {
+    id: 25,
+    name: {
+      first: 'Lily',
+      last: 'Ross',
+    },
+    email: 'lily@ross.com',
+  },
+  {
+    id: 26,
+    name: {
+      first: 'James',
+      last: 'Perry',
+    },
+    email: 'james@perry.com',
+  },
+  {
+    id: 27,
+    name: {
+      first: 'Aiden',
+      last: 'Evans',
+    },
+    email: 'aiden@evans.com',
+  },
+  {
+    id: 28,
+    name: {
+      first: 'Natalie',
+      last: 'Fisher',
+    },
+    email: 'natalie@fisher.com',
+  },
+  {
+    id: 29,
+    name: {
+      first: 'Logan',
+      last: 'Barnes',
+    },
+    email: 'logan@barnes.com',
+  },
+  {
+    id: 30,
+    name: {
+      first: 'Avery',
+      last: 'Reed',
+    },
+    email: 'avery@reed.com',
+  },
+  {
+    id: 31,
+    name: {
+      first: 'Jackson',
+      last: 'Cole',
+    },
+    email: 'jackson@cole.com',
+  },
+  {
+    id: 32,
+    name: {
+      first: 'Ella',
+      last: 'Ferguson',
+    },
+    email: 'ella@ferguson.com',
+  },
+  {
+    id: 33,
+    name: {
+      first: 'Carter',
+      last: 'Owens',
+    },
+    email: 'carter@owens.com',
+  },
+  {
+    id: 34,
+    name: {
+      first: 'Addison',
+      last: 'Harrison',
+    },
+    email: 'addison@harrison.com',
+  },
+  {
+    id: 35,
+    name: {
+      first: 'Samuel',
+      last: 'Graham',
+    },
+    email: 'samuel@graham.com',
+  },
 ];
